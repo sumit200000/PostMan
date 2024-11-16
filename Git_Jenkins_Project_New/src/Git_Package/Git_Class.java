ebc729912f71c3b9ef69970a1a038a55297c8fb7
Sumit Sharma <2001ssumit@gmail.com>
2024-11-16 12:30:56 +0800

    Signed-off-by: Sumit Sharma <2001ssumit@gmail.com>

--- Git_Jenkins_Project_New/src/Git_Package/Git_Class.java
+++ Git_Jenkins_Project_New/src/Git_Package/Git_Class.java
@@ -1,9 +1,11 @@
+package Git_Package;
 
-public class Git_Project {
+public class Git_Class {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Git");
 	}
 
 }
